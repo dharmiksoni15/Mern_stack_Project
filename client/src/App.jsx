f200eda7ead72b02935606ab4c40637303cc9373
Dharmik Soni <sonidharmik21@gmail.com>
2026-03-20 18:04:07 +0530

    feat(frontend): initialize React app with Vite and setup routing
    - Installed and configured React with Vite
    - Added React Router DOM for page navigation
    - Created basic pages: Home, About, Contact, Service, Login, Register
    - Implemented routing structure in App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Service from "./pages/Service";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/service" element={<Service />} />
+
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
