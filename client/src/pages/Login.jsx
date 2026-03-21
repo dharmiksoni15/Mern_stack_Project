7b997de2da95bfec5c549f60557441a24a6fc3cc
Dharmik Soni <sonidharmik21@gmail.com>
2026-03-21 14:54:51 +0530

    Add login page with UI styling and state management

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,60 @@
-import React from "react";
+import { useState } from "react";
+import "./Login.css";
 
 const Login = () => {
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
   return (
-    <div>
-      <h1>Login Component</h1>
-    </div>
+    <>
+      <div className="login-container">
+
+        <h1>Login Form</h1>
+
+        <form onSubmit={handleSubmit}>
+
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            value={user.email}
+            onChange={handleInput}
+          />
+
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            value={user.password}
+            onChange={handleInput}
+          />
+
+          <button type="submit">Login</button>
+
+        </form>
+
+      </div>
+    </>
   );
 };
 
